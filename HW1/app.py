020575a1720abc013d562e9a0bde236fd8eac763
Jieon21 <cekje24@gmail.com>
2026-09-22 16:54:22 +0900

    feat: HW1 프로필(취미) 페이지 추가

--- HW1/app.py
+++ HW1/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", name="김지언", student_id="24012016")
 
+@app.route("/profile")
+def profile():
+    hobbies = ["독서", "영화 감상", "코딩"]
+    return render_template("profile.html", hobbies=hobbies)
+
 if __name__ == "__main__":
     app.run(debug=True)
